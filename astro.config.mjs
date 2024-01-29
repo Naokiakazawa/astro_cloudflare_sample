14aaf71ba5a91417ee89c3c8ca9c7a93feed30af
Naokiakazawa <naoki9akazawa@gmail.com>
2024-01-29 13:03:47 +0900

    add robotTXT package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  site: 'https://astro-cloudflare-sample.pages.dev',
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 });
